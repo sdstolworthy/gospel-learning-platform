732d3ab19d5aa8fb2754e0ab12fc4e8da8bd2452
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-10 17:55:15 -0600

    bugfixes

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,16 +1,12 @@
-import * as React from 'react';
+import * as React from 'react'
 
 import {
-  Button,
-  Control,
-  Field,
   Icon,
   Navbar,
   NavbarBrand,
   NavbarBurger,
   NavbarDivider,
   NavbarDropdown,
-  NavbarEnd,
   NavbarItem,
   NavbarLink,
   NavbarMenu,
@@ -24,10 +20,10 @@ interface IDefaultLayoutProps {
 export default function DefaultLayout({ children }: IDefaultLayoutProps) {
   return (
     <div>
-      <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
+      <Navbar className="is-dark is-fixed">
         <NavbarBrand>
             <NavbarItem>
-                <img src={''} style={{ marginRight: 5 }} /> Bloomer
+                <img src={''} style={{ marginRight: 5 }} /> Gospel Learning Platform
             </NavbarItem>
             <NavbarItem isHidden='desktop'>
                 <Icon className='fa fa-github' />
@@ -50,26 +46,6 @@ export default function DefaultLayout({ children }: IDefaultLayoutProps) {
                     </NavbarDropdown>
                 </NavbarItem>
             </NavbarStart>
-            <NavbarEnd>
-                <NavbarItem href="https://github.com/AlgusDark/bloomer" isHidden='touch'>
-                    <Icon className='fa fa-github' />
-                </NavbarItem>
-                <NavbarItem href="https://twitter.com/AlgusDark" isHidden='touch'>
-                    <Icon className='fa fa-twitter' style={{ color: '#55acee' }} />
-                </NavbarItem>
-                <NavbarItem>
-                    <Field isGrouped={true}>
-                        <Control>
-                            <Button id="twitter" data-social-network="Twitter" data-social-action="tweet"
-                            data-social-target="http://bloomer.js.org" target="_blank" href="https://twitter.com/intent/tweet?text=bloomer:
-                            a set of React Stateless Components for bulma.io&amp;url=http://bloomer.js.org&amp;via=AlgusDark">
-                                <Icon className="fa fa-twitter" />
-                                <span>Tweet</span>
-                            </Button>
-                        </Control>
-                    </Field>
-                </NavbarItem>
-            </NavbarEnd>
         </NavbarMenu>
       </Navbar>
       {children}
--- src/routes/ClassSchedule/components/Menu.tsx
+++ src/routes/ClassSchedule/components/Menu.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from 'react'
 
 import {
   Menu,
@@ -13,48 +13,10 @@ export default function ScheduleMenu({}) {
       <MenuLabel>General</MenuLabel>
       <MenuList>
         <li>
-          <MenuLink>Dashboard</MenuLink>
+          <MenuLink isActive={true}>Class Schedule</MenuLink>
         </li>
         <li>
-          <MenuLink>Customers</MenuLink>
-        </li>
-      </MenuList>
-      <MenuLabel>Administration</MenuLabel>
-      <MenuList>
-        <li>
-          <MenuLink>Team Settings</MenuLink>
-        </li>
-        <li>
-          <MenuLink isActive={true}>Manage Your Team</MenuLink>
-        </li>
-        <MenuList>
-          <li>
-            <MenuLink>Members</MenuLink>
-          </li>
-          <li>
-            <MenuLink>Plugins</MenuLink>
-          </li>
-          <li>
-            <MenuLink>Add a Member</MenuLink>
-          </li>
-        </MenuList>
-        <li>
-          <MenuLink>Invitations</MenuLink>
-        </li>
-        <li>
-          <MenuLink>Authentication</MenuLink>
-        </li>
-      </MenuList>
-      <MenuLabel>Transactions</MenuLabel>
-      <MenuList>
-        <li>
-          <MenuLink>Payments</MenuLink>
-        </li>
-        <li>
-          <MenuLink>Transfers</MenuLink>
-        </li>
-        <li>
-          <MenuLink>Balance</MenuLink>
+          <MenuLink>Class Members</MenuLink>
         </li>
       </MenuList>
     </Menu>
